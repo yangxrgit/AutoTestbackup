7b7848ebcf71645eb7c6ed47156a72c2ebd2e7d2
yangxr <467636649@qq.com>
2018-08-05 13:14:45 +0800

    注解2

--- chapter5/src/main/java/com/course/testng/BasicAnnotation.java
+++ chapter5/src/main/java/com/course/testng/BasicAnnotation.java
@@ -1,8 +1,6 @@
 package com.course.testng;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class BasicAnnotation {
 
@@ -24,5 +22,25 @@ public class BasicAnnotation {
     @AfterMethod
     public void afterMethod(){
         System.out.println("AfterMethod这是在测试方法之后运行的");
+    }
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("BeforeClass这是在类之前运行的");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("afterClass这是在类之前运行的");
+    }
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("beforeSuite测试套件");
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("afterSuite测试套件");
     }
 }
